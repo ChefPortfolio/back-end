f4d440a9e3ba4030ee7fb83e8573f0e8cf747234
Blake <bpro0821@gmail.com>
2019-09-24 12:00:02 -0500

    adds seed data

--- database/seeds/001-chefs.js
+++ database/seeds/001-chefs.js
@@ -1,13 +1,15 @@
 
 exports.seed = function(knex) {
   // Deletes ALL existing entries
-  return knex('table_name').del()
+  return knex('chefs').del()
     .then(function () {
       // Inserts seed entries
-      return knex('table_name').insert([
-        {id: 1, colName: 'rowValue1'},
-        {id: 2, colName: 'rowValue2'},
-        {id: 3, colName: 'rowValue3'}
+      return knex('chefs').insert([
+        {id: 1, first_name: 'John',last_name:'Monfriez', location:'Los Angelas', contact:'https://www.linkedin.com/in/johnny-5/', username:'johnny5', password: 'courage', email_address:'night_train@chefs.com', avatar_url: ''},
+        {id: 2, first_name: 'Alexis',last_name:'Grey', location:'Seattle', contact:'https://www.linkedin.com/in/alexis-grey/', username:'alexisg', password: 'medicine', email_address:'alexis_grey@chef.com', avatar_url: ''},
+        {id: 3, first_name: 'Charlee',last_name:'Holden', location:'Denver', contact:'https://www.facebook.com/in/alexis-grey/', username:'charhold', password: 'bones', email_address:'charlee_holden@chef.com', avatar_url: ''},
+        {id: 4, first_name: 'Bailey',last_name:'Beetle', location:'Chicago', contact:'https://www.facebook.com/in/alexis-grey/', username:'bailey123', password: 'anatomy', email_address:'beetle_bailey@chef.com', avatar_url: ''},
+        {id: 5, first_name: '',last_name:'', location:'', contact:'', username:'', password: '', email_address:'', avatar_url: ''}
       ]);
     });
 };
