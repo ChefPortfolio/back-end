74e60354420ded8b5f811cff4b3fcce8c1807da6
Blake <bpro0821@gmail.com>
2019-09-24 17:58:52 -0500

    code-cleaner

--- chefs/chefs-model.js
+++ chefs/chefs-model.js
@@ -11,14 +11,14 @@ module.exports = {
 
 function getChefs() {
   return db('chefs');
-}
+};
 function getById(id) {
   return db('chefs').where({ id });
-}
+};
 
 function add(chef) {
   return db('chefs').insert(chef);
-}
+};
 
 function getChefRecipes(id) {
   return db('recipes as r')
@@ -26,16 +26,16 @@ function getChefRecipes(id) {
   .select('r.title','r.description','r.instructions','r.meal_type','r.pic_url')
   .where({ 'r.chef_id':id })
   
-}// needs tweak
+};
 
 function update(id, changes) {
   return db('chefs')
     .where({ id })
     .update(changes);
-}
+};
 
 function remove(id) {
   return db('chefs')
     .where({ id })
     .del();
-}
+};
