00b5d11007806a45b69777b74f42e1f98b651531
Blake <bpro0821@gmail.com>
2019-09-25 16:40:49 -0500

    updates to knex for pg fix attempts

--- knexfile.js
+++ knexfile.js
@@ -21,7 +21,8 @@ module.exports = {
   },
   production: {
     client: 'pg',
-    connection: process.env.DATABASE_URL,
+    useNullAsDefault: true,
+    connection: process.env.DATABASE_URL `?ssl=true`,
     migrations: {
       directory: './database/migrations',
     },
