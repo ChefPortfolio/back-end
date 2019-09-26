541c67bb1e31d8ce4d1f2864349c009df56e7b52
Lambda_School_Loaner_112 <lambda_school_loaner_112@Lambda-School-Loaner-112s-MacBook-Pro.local>
2019-09-26 00:05:29 -0500

    13

--- database/seeds/001-chefs.js
+++ database/seeds/001-chefs.js
@@ -1,7 +1,7 @@
 
 exports.seed = function(knex, Promise) {
   // Deletes ALL existing entries
-  return knex('chefs').truncate()
+  return knex('chefs').del()
     .then(function () {
       // Inserts seed entries
       return knex('chefs').insert([
