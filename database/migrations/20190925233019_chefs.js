87ae38fea0895405a0d307c448bc5791a004325f
Lambda_School_Loaner_112 <lambda_school_loaner_112@Lambda-School-Loaner-112s-MacBook-Pro.local>
2019-09-25 23:37:25 -0500

    11

--- database/migrations/20190925233019_chefs.js
+++ database/migrations/20190925233019_chefs.js
@@ -3,7 +3,7 @@
 exports.up = function(knex, Promise) {
  
     return knex.schema.createTable('chefs',tbl => {
-        tbl.increments().onUpdate().onUpdate();
+        tbl.increments();
         tbl.string('first_name').notNullable();
         tbl.string('last_name').notNullable();
         tbl.string('location');
@@ -42,8 +42,10 @@ exports.up = function(knex, Promise) {
   
   exports.down = function(knex, Promise) {
     return knex.schema
-    .dropTableIfExists('measurement_recipe_ingredient')
-    .dropTableIfExists('ingredients')
     .dropTableIfExists('chefs')
     .dropTableIfExists('recipes')
+    .dropTableIfExists('measurement_recipe_ingredient')
+    .dropTableIfExists('ingredients')
+    
+    
     };
--- database/seeds/001-chefs.js
+++ database/seeds/001-chefs.js
@@ -1,7 +1,7 @@
 
 exports.seed = function(knex, Promise) {
   // Deletes ALL existing entries
-  return knex('chefs').del()
+  return knex('chefs').truncate()
     .then(function () {
       // Inserts seed entries
       return knex('chefs').insert([
