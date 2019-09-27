5a87230346520b43543e6123afe99d4ea574ebc5
Blake <bpro0821@gmail.com>
2019-09-26 20:49:34 -0500

    postgres rulz

--- auth/auth-router.js
+++ auth/auth-router.js
@@ -52,7 +52,7 @@ router.post('/login', (req, res) => {
         const userId = user.id
         if (user && bcrypt.compareSync(password, user.password)) {
           const token = generateToken(user)
-          res.status(200).json({ message: `Welcome ${user.username}!`,token, userId });
+          res.status(200).json({ message: `Welcome ${user.username}!`, token: token, id:userId });
         } else {
           res.status(401).json({ message: 'Invalid Credentials' });
         }
